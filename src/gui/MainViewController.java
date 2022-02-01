2b212eb91aa2ed1c3eabb279b7a299717a7858c3
Andre Avelino da Silva <andreavelinosilva@hotmail.com>
2022-02-01 15:02:13 -0300

    Initializing action as parameter

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -3,6 +3,7 @@ package gui;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
 
 import application.Main;
 import gui.util.Alertas;
@@ -34,12 +35,15 @@ public class MainViewController implements Initializable { // classe de controle
 	
 	@FXML
 	public void onMenuItemDepartamentoAction() {// método para o menu do departamento que vai gerar uma ação na tela
-		loadView2("/gui/ListaDepartamento.fxml");
+		loadView("/gui/ListaDepartamento.fxml", (DepartmentListController controller) -> {
+			controller.setDepartmentServices(new DepartmentService());
+			controller.updateTableView();
+		});
 	}
 	
 	@FXML
 	public void onMenuItemAboutAction() { // método para o menu do about que vai gerar uma ação na tela
-		loadView("/gui/About.fxml");
+		loadView("/gui/About.fxml", x -> {});
 	}
 	
 	
@@ -53,65 +57,39 @@ public class MainViewController implements Initializable { // classe de controle
 	 * Método responsavel por carregar uma janela dentro da outra, sendo o About dentro da janela principal.
 	 */
 	
-	private synchronized void loadView(String absoluteName) { 
+	private synchronized <T> void loadView(String absoluteName, Consumer<T> initializingAction ) { 
 		try {
-		FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName)); // Estanciar uma nova tela
-		VBox newVBox = loader.load(); // tela nova sendo carregado como o novo vbox do menu About
-		
-		Scene mainScene = Main.getMainScene(); // chamar a tela principal da classe Main guardando ela na variavel mainScene.
-		VBox mainVBox =  (VBox) ((ScrollPane) mainScene.getRoot()).getContent(); // Acessando o VBox da classe Main, chamando 
-		// o mainScene ja declarado, com o comando getRoot() que traz o primeiro elemento da mainScene la da Classe Main, neste caso
-		// o 1º item é o SCrollPane então a necessidade do Casting (ScrollPane), logo depois chama o comando get.Content(), que é o elemento que esta
-		// dentro do ScroolPane, quando voce chama o Content você fala para o compilador que quer o que esta dentro dele que é o VBox, ai
-		// basta fazer mais um casting (VBox) e o compilador entenderá que você quer só o Vboc.
-		
-		Node mainMenu = mainVBox.getChildren().get(0); // Vamo extrair agora os filhos do vBox com o comando mainVBox.getChildren() (que são os menus)
-		// e guarda-los na variavel mainMenu.
-		
-		mainVBox.getChildren().clear(); // logo após extrai-los vamos apagar os filhos do mainVBox, com o comando mainVBoc.getChildren()
-		
-		mainVBox.getChildren().add(mainMenu); // Depois de Excluir, vamos adiciona-los novamente, porém desta vez só os menus que estão na 
-		// variavel mainmenu.
-		mainVBox.getChildren().addAll(newVBox.getChildren()); // Agora vamos incluir uma coleção com este comando, para inserirmos os menus
-		// que são os filhos da variavel newVBox então tudo ficará funcional os menus da newVBox e os menus da VBox anterior. 
-		
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName)); // Estanciar uma nova tela
+			VBox newVBox = loader.load(); // tela nova sendo carregado como o novo vbox do menu About
+			
+			Scene mainScene = Main.getMainScene(); // chamar a tela principal da classe Main guardando ela na variavel mainScene.
+			VBox mainVBox =  (VBox) ((ScrollPane) mainScene.getRoot()).getContent(); // Acessando o VBox da classe Main, chamando 
+			// o mainScene ja declarado, com o comando getRoot() que traz o primeiro elemento da mainScene la da Classe Main, neste caso
+			// o 1º item é o SCrollPane então a necessidade do Casting (ScrollPane), logo depois chama o comando get.Content(), que é o elemento que esta
+			// dentro do ScroolPane, quando voce chama o Content você fala para o compilador que quer o que esta dentro dele que é o VBox, ai
+			// basta fazer mais um casting (VBox) e o compilador entenderá que você quer só o Vboc.
+			
+			Node mainMenu = mainVBox.getChildren().get(0); // Vamo extrair agora os filhos do vBox com o comando mainVBox.getChildren() (que são os menus)
+			// e guarda-los na variavel mainMenu.
+			
+			mainVBox.getChildren().clear(); // logo após extrai-los vamos apagar os filhos do mainVBox, com o comando mainVBoc.getChildren()
+			
+			mainVBox.getChildren().add(mainMenu); // Depois de Excluir, vamos adiciona-los novamente, porém desta vez só os menus que estão na 
+			// variavel mainmenu.
+			mainVBox.getChildren().addAll(newVBox.getChildren()); // Agora vamos incluir uma coleção com este comando, para inserirmos os menus
+			// que são os filhos da variavel newVBox então tudo ficará funcional os menus da newVBox e os menus da VBox anterior. 
+			
+			
+			T controller = loader.getController();
+			initializingAction.accept(controller);
+			
+			
+			}
+			catch(IOException e) {
+				Alertas.showAlert("IO Exception", "Erro de carregamento de pagina", e.getMessage(), AlertType.ERROR);
+			}
 		}
-		catch(IOException e) {
-			Alertas.showAlert("IO Exception", "Erro de carregamento de pagina", e.getMessage(), AlertType.ERROR);
-		}
-	}
 	
-	private synchronized void loadView2(String absoluteName) { 
-		try {
-		FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName)); // Estanciar uma nova tela
-		VBox newVBox = loader.load(); // tela nova sendo carregado como o novo vbox do menu About
-		
-		Scene mainScene = Main.getMainScene(); // chamar a tela principal da classe Main guardando ela na variavel mainScene.
-		VBox mainVBox =  (VBox) ((ScrollPane) mainScene.getRoot()).getContent(); // Acessando o VBox da classe Main, chamando 
-		// o mainScene ja declarado, com o comando getRoot() que traz o primeiro elemento da mainScene la da Classe Main, neste caso
-		// o 1º item é o SCrollPane então a necessidade do Casting (ScrollPane), logo depois chama o comando get.Content(), que é o elemento que esta
-		// dentro do ScroolPane, quando voce chama o Content você fala para o compilador que quer o que esta dentro dele que é o VBox, ai
-		// basta fazer mais um casting (VBox) e o compilador entenderá que você quer só o Vboc.
-		
-		Node mainMenu = mainVBox.getChildren().get(0); // Vamo extrair agora os filhos do vBox com o comando mainVBox.getChildren() (que são os menus)
-		// e guarda-los na variavel mainMenu.
-		
-		mainVBox.getChildren().clear(); // logo após extrai-los vamos apagar os filhos do mainVBox, com o comando mainVBoc.getChildren()
-		
-		mainVBox.getChildren().add(mainMenu); // Depois de Excluir, vamos adiciona-los novamente, porém desta vez só os menus que estão na 
-		// variavel mainmenu.
-		mainVBox.getChildren().addAll(newVBox.getChildren()); // Agora vamos incluir uma coleção com este comando, para inserirmos os menus
-		// que são os filhos da variavel newVBox então tudo ficará funcional os menus da newVBox e os menus da VBox anterior. 
-		
-		DepartmentListController controller = loader.getController();
-		controller.setDepartmentServices(new DepartmentService());
-		controller.updateTableView();
-		
-		
-		}
-		catch(IOException e) {
-			Alertas.showAlert("IO Exception", "Erro de carregamento de pagina", e.getMessage(), AlertType.ERROR);
-		}
-	}
+	
 	
 }
